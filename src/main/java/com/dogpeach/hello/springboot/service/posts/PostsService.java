51e652aff8ee07486dbcbf3ab34697870897ac32
Taeyang Kim <mynamesunpower@gmail.com>
2021-07-28 13:05:03 +0900

    Jpa Posts update / select

--- src/main/java/com/dogpeach/hello/springboot/service/posts/PostsService.java
+++ src/main/java/com/dogpeach/hello/springboot/service/posts/PostsService.java
@@ -1,9 +1,13 @@
 package com.dogpeach.hello.springboot.service.posts;
 
+import com.dogpeach.hello.springboot.domain.posts.Posts;
 import com.dogpeach.hello.springboot.domain.posts.PostsRepository;
+import com.dogpeach.hello.springboot.web.dto.PostsResponseDto;
 import com.dogpeach.hello.springboot.web.dto.PostsSaveRequestDto;
+import com.dogpeach.hello.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
@@ -11,7 +15,23 @@ public class PostsService {
 
     private final PostsRepository postsRepository;
 
+    @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+    
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
     }
 }
