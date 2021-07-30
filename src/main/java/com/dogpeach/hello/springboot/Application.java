290b9c838ec86f88f2db1115490f18da2bc49b69
Taeyang Kim <mynamesunpower@gmail.com>
2021-07-30 13:29:35 +0900

    WebMVCTest를 위해 EnableJpaAuditing 분리

--- src/main/java/com/dogpeach/hello/springboot/Application.java
+++ src/main/java/com/dogpeach/hello/springboot/Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
+//@EnableJpaAuditing 이것을 사용하기 위해서 최소 하나의 @Entity 클래스 필요.
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
