89e33449cdec632ef2d5cc7446017d64b0e055e8
mynamesunpower <mynamesunpower@gmail.com>
2021-08-02 22:37:33 +0900

    deploy.sh 코드 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,7 +7,8 @@ echo "> Build 파일 복사"
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동 중인 어플리케이션 pid 확인"
-CURRENT_PID=$(pgrep -fl hello-boot | grep jar | awk '{print $1}')
+#CURRENT_PID=$(pgrep -fl hello-boot | grep jar | awk '{print $1}')
+CURRENT_PID=$(ps -ef | grep ${PROJECT_NAME} | grep jar | awk '{print $2}')
 
 echo ">현재 구동 중인 어플리케이션 pid: $CURRENT_PID"
 
